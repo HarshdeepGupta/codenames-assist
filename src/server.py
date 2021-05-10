de7271544291baefb4f9e7a82e122c08bfd8fbea
Harshdeep Gupta <hag@microsoft.com>
2021-05-09 20:08:33 -0700

    Add a simple form to get a single word

--- src/server.py
+++ src/server.py
@@ -1,20 +1,29 @@
 from datetime import datetime
-from flask import Flask, render_template
-server = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
 
 
-@server.route("/")
+@app.route("/")
 def hello():
-    return "Hello World!"
+    app.logger.info(("name is: {}".format(app.name)))
+    return redirect("home")
 
 
-@server.route("/home/")
-def hello_there(name=None):
-    server.logger.error("THis is it")
-    return render_template(
-        "home.html"
-    )
+@app.route("/home/", methods=['GET', 'POST'])
+def homepage(name=None):
+    if request.method == "POST":
+        words = request.form["nm"]
+        return redirect(url_for("getClues", words=words))
+    else:
+        return render_template(
+            "home.html"
+        )
+
+
+@app.route('/getClues/<words>')
+def getClues(words: str):
+    return "The words are: {}".format(words.split(','))
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0",  debug=True, port=5000)
+    app.run(host="0.0.0.0",  debug=True, port=5000)
