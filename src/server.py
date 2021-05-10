ad23357b6f404d6e97e20b20bbc96facb3660253
Harshdeep Gupta <hag@microsoft.com>
2021-05-09 19:07:48 -0700

    Modify paths

--- src/server.py
+++ src/server.py
@@ -1,21 +1,20 @@
+from datetime import datetime
 from flask import Flask, render_template
 server = Flask(__name__)
-from datetime import datetime
+
 
 @server.route("/")
 def hello():
-   return "Hello World!"
+    return "Hello World!"
 
 
-@server.route("/hello/")
-@server.route("/hello/<name>")
-def hello_there(name = None):
+@server.route("/home/")
+def hello_there(name=None):
     server.logger.error("THis is it")
     return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
+        "home.html"
     )
 
+
 if __name__ == "__main__":
-   server.run(host='0.0.0.0')
+    server.run(host="0.0.0.0",  debug=True, port=5000)
