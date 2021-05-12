1bb40be39c6fc4778550f77c3761e31e12afb6aa
Harshdeep Gupta <hag@microsoft.com>
2021-05-12 01:50:14 -0700

    Make a basic page to take in form data

--- src/server.py
+++ src/server.py
@@ -1,35 +1,31 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, session
+from config import config
+from Logger import Logger
 app = Flask(__name__)
-app.secret_key = "hello"
+app.config.from_object(config)
+logger = Logger(app)
+
 
 
 @app.route("/")
 def hello():
-    app.logger.info(("name is: {}".format(app.name)))
+    logger.log(("name is: {}".format(app.name)))
     return redirect("home")
 
 
 @app.route("/home/", methods=['GET', 'POST'])
-def homepage(name=None):
-    if request.method == "POST":
-        words = request.form["nm"]
-        if len(words):
-            session["words"] = words
-        return redirect(url_for("getClues"))
-    else:
-        return render_template(
-            "home.html"
-        )
+def homepage():
+    return render_template(
+        "home.html"
+    )
 
 
-@app.route('/getClues')
-def getClues():
-    if "words" in session:
-        words = session['words']
-        return "The words are: {}".format(words.split(','))
-    else:
-        return redirect("home")
+@app.route('/results', methods=["POST"])
+def results():
+    listOfWords: str = request.form.get("listOfWords")
+    results = listOfWords.split(",")
+    return render_template("results.html", results=results)
 
 
 if __name__ == "__main__":
